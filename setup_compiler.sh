0a5d724af880ae014d090c2f75eb29d4a0094f9f
Paulo Barcelos <paulo@azucrina.org>
2017-12-15 16:42:19 +0100

    Updated script

--- setup_compiler.sh
+++ setup_compiler.sh
@@ -52,7 +52,7 @@ cat "$BASE/output.txt"| grep "firmware.ino.elf" | grep -v "firmware.ino.eep" >> 
 
 # tracefile all the used files
 perl "$ROOT/tracefile.perl" -uef sh "$BASE/build.sh" | grep $BASE >> "$BASE/rawtrace"
-cat "$BASE/rawtrace" | xargs -n1 realpath >> "$BASE/trace"
+cat "$BASE/rawtrace" | xargs -n1 echo >> "$BASE/trace"
 cat "$BASE/trace"
 
 # delete all unused filesf
