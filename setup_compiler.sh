4a6fdea4f2abe2dd9c8b96224e7600a22b78c504
Paulo Barcelos <paulo@azucrina.org>
2017-12-16 16:47:51 +0100

    updated script

--- setup_compiler.sh
+++ setup_compiler.sh
@@ -82,7 +82,7 @@ rm -rf "$BASE/build/firmware.ino.hex"
 # update the home path to the location within the new image
 echo "#########################################################################"
 echo "updating home path"
-sed -i "s|$ROOT|/home|g" "$BASE/*"
+sed -i "s|$ROOT|/home|g" *
 cat "$BASE/build.sh"
 
 # compress the compiler
