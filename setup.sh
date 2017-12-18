139d0e36ca88bd78bd0ff3300613753344e47311
Paulo Barcelos <paulo@azucrina.org>
2017-12-18 16:08:31 +0100

    update

--- setup.sh
+++ setup.sh
@@ -2,10 +2,17 @@ sudo apt-get -qq update
 sudo apt-get --yes --force-yes install xorriso
 sudo apt-get --yes --force-yes install realpath
 sudo apt-get --yes --force-yes install tree
-curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
-sudo apt-get install -y nodejs
 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+nvm install 9.2.0
+npm --version
+nvm --version
 npm install
+
+npm install quirkbot-arduino-builder@0.0.5
+npm install quirkbot-arduino-hardware@0.4.9
+npm install quirkbot-arduino-library@2.5.7
+npm install quirkbot-avr-gcc@1.0.1
 
 bash setup_compiler.sh
 bash custom-tinycore.sh
