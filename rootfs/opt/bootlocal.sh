3b8ae154da8915789124dc19e23d104c4bdc4a3f
Paulo Barcelos <paulo@azucrina.org>
2017-12-18 18:22:32 +0100

    update

--- rootfs/opt/bootlocal.sh
+++ rootfs/opt/bootlocal.sh
@@ -8,11 +8,11 @@ do
 	if [ ! -z "$IN" ]
 	then
 		echo "go!"
-		echo $IN > /home/tc/compiler/build/sketch/firmware.ino.cpp
-		sed -i.bak s/--nl--/\\n/g /home/tc/compiler/build/sketch/firmware.ino.cpp
-		sh /home/tc/compiler/build.sh
+		echo $IN > /compiler/build/sketch/firmware.ino.cpp
+		sed -i.bak s/--nl--/\\n/g /compiler/build/sketch/firmware.ino.cpp
+		sh /compiler/build.sh
 		echo "--hex--" > /dev/ttyS0
-		cat /home/tc/compiler/build/firmware.ino.hex > /dev/ttyS0
+		cat /compiler/build/firmware.ino.hex > /dev/ttyS0
 		echo "--out--" > /dev/ttyS0
 		#cat compileoutput > /dev/ttyS0
 		#cat sizeoutput > /dev/ttyS0
