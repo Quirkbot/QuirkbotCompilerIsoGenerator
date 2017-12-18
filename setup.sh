5e0382fda0f9c8230d1d9517d33bf08d27bbd42f
Paulo Barcelos <paulo@azucrina.org>
2017-12-18 16:03:56 +0100

    update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 sudo apt-get -qq update
-sudo apt-get install xorriso
-sudo apt-get install realpath
-sudo apt-get install tree
+sudo apt-get --yes --force-yes install xorriso
+sudo apt-get --yes --force-yes install realpath
+sudo apt-get --yes --force-yes install tree
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 npm install
 
 bash setup_compiler.sh
