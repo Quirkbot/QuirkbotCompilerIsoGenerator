e9075b0f5c9037e526e8410eee47496693660c8b
Paulo Barcelos <paulo@azucrina.org>
2017-12-18 16:14:27 +0100

    update

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ sudo apt-get --yes --force-yes install tree
 sudo apt-get --yes --force-yes install curl
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 nvm install 9.2.0
 npm --version
 nvm --version
