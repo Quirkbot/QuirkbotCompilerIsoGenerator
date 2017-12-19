4bdd8b4a93f419635a59af2401f6601e7d99ae63
Paulo Barcelos <paulo@azucrina.org>
2017-12-19 15:20:35 +0100

    tinycore fiels

--- setup_tinycore.sh
+++ setup_tinycore.sh
@@ -1,6 +1,6 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH
 
-bash setup_tinycore_dependencies.sh
+sh setup_tinycore_dependencies.sh
 bash setup_compiler.sh
-bash setup_image.sh
+sudo bash setup_image.sh
